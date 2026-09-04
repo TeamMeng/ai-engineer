c5784ab19bad7d5e39aa76d26e5cd166d96e3916
TeamMeng <862915265@qq.com>
2026-09-04 16:11:07 +0800

    feat: looking forword Sandbox message

--- hello-sandbox/src/main.rs
+++ hello-sandbox/src/main.rs
@@ -3,32 +3,22 @@ use microsandbox::Sandbox;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let sb = Sandbox::builder("Hello")
-        .replace()
-        .image("docker.m.daocloud.io/library/python:3.12-alpine")
-        .memory(512)
-        .create()
-        .await?;
+    let sb = Sandbox::list().await?;
 
-    let handle = Sandbox::get("Hello").await?;
+    if sb.sandboxes.is_empty() {
+        println!("Not found sandbox");
+        return Ok(());
+    }
 
-    // 1. 查看沙箱名称
-    println!("沙箱名称: {}", handle.name());
-    // 2. 查看沙箱状态
-    // - Handle 上提供同步快照方法：status_snapshot()
-    println!("沙箱状态(快照): {:?}", handle.status_snapshot());
-    // - live 沙箱对象上提供异步实时查询：sb.status().await?
-    println!("沙箱状态(实时): {:?}", sb.status().await?);
+    println!("There have {} sandboxes", sb.sandboxes.len());
 
-    // 3. 查看内存配置
-    // - handle.config() 返回 Result<SandboxConfig>，需加 `?` 解包
-    // - memory_mib 位于 spec.resources 结构体中
-    println!(
-        "内存配置: {} MiB",
-        handle.config()?.spec.resources.memory_mib
-    );
-
-    sb.destroy().await?;
+    for handle in &sb.sandboxes {
+        println!(
+            "name: {}, status: {:?}",
+            handle.name(),
+            handle.status_snapshot()
+        );
+    }
 
     Ok(())
 }
