d1b324292815f01014147e63147114441d692b98
TeamMeng <862915265@qq.com>
2026-09-04 20:27:38 +0800

    feat: support read file

--- hello-sandbox/src/main.rs
+++ hello-sandbox/src/main.rs
@@ -6,33 +6,14 @@ async fn main() -> Result<()> {
     let sb = Sandbox::builder("Hello")
         .replace()
         .image("docker.m.daocloud.io/library/python:3.12-alpine")
-        .memory(512)
         .create()
         .await?;
 
-    let output = sb
-        .exec(
-            "python",
-            [
-                "-c",
-                r#"import sys
-print('这是标准输出')
-print('这是错误输出', file=sys.stderr)
-sys.exit(0)"#,
-            ],
-        )
+    sb.shell("dd if=/dev/urandom of=/tmp/result.bin bs=1024 count=4")
         .await?;
 
-    let stdout_text = output.stdout()?;
-    println!("stdout: {}", stdout_text);
-
-    let stderr_text = output.stderr()?;
-    println!("stderr: {}", stderr_text);
-
-    let exit_code = output.status().code;
-    let is_success = output.status().success;
-
-    println!("exit code: {}, is success: {}", exit_code, is_success);
+    let bytes = sb.fs().read("/tmp/result.bin").await?;
+    println!("read {} bytes", bytes.len());
 
     sb.destroy().await?;
 
